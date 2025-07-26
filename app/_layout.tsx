23866922dc275cc212518fd8c5e0c45d2f74442f
Jarry1222 <William-Champion@outlook.com>
2025-07-25 21:56:15 -0300

    diseño de la calculadora listo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,23 @@
+import { Colors } from "@/constants/Colors";
+import { useFonts } from "expo-font";
 import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text, View } from "react-native";
 const RootLayout = () => {
+
+  const [loaded] = useFonts({
+    SpaceMono : require("../assets/fonts/SpaceMono-Regular.ttf")
+  })
+
+  if(!loaded){
+    return null
+  }
   return (
-    <View>
+    <View style={{ backgroundColor : Colors.background , flex:1}}>
       <Text>Header</Text>
         <Slot/>
-        <Text>Footer</Text>
+       <StatusBar style="light"/>
     </View>
   );
 };
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,67 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import CalculatorBottom from "@/components/CalculatorBottom";
+import ThemeText from "@/components/ThemeText";
+import { Colors } from "@/constants/Colors";
+import { globalStyles } from "@/styles/global-styles";
+import React from "react";
+import { View } from "react-native";
 
 const Calculator = () => {
   return (
-    <View>
-      <Text>Calculator</Text>
-    </View>
-  )
-}
+    <View style={globalStyles.calculatorContainer}>
+      <View style={{ paddingHorizontal: 30, marginBottom: 20 }}>
+        <ThemeText variant="h1">50 x 50</ThemeText>
+        <ThemeText variant="h2">250</ThemeText>
+      </View>
 
-export default Calculator
+      {/* Filas de Botones¨*/}
+
+      <View style={globalStyles.row}>
+        <CalculatorBottom label="C" onPress={() => console.log("HOLA CLIK")} blackText color={Colors.lightGray} />
+        <CalculatorBottom label="+/-" onPress={() => console.log("HOLA CLIK")} blackText color={Colors.lightGray} />
+        <CalculatorBottom label="del" onPress={() => console.log("HOLA CLIK")} blackText color={Colors.lightGray} />
+        <CalculatorBottom label="/" onPress={() => console.log("HOLA CLIK")}  color={Colors.orange} />
+      </View>
+
+        {/*Segunda Filas de Botones¨*/}
+
+      <View style={globalStyles.row}>
+        <CalculatorBottom label="1" onPress={() => console.log("HOLA CLIK")}   />
+        <CalculatorBottom label="2" onPress={() => console.log("HOLA CLIK")}   />
+        <CalculatorBottom label="3" onPress={() => console.log("HOLA CLIK")}   />
+        <CalculatorBottom label="*" onPress={() => console.log("HOLA CLIK")}  color={Colors.orange} />
+      </View>
+
+       {/*Tercera Filas de Botones¨*/}
+
+      <View style={globalStyles.row}>
+        <CalculatorBottom label="4" onPress={() => console.log("HOLA CLIK")}   />
+        <CalculatorBottom label="5" onPress={() => console.log("HOLA CLIK")}   />
+        <CalculatorBottom label="6" onPress={() => console.log("HOLA CLIK")}   />
+        <CalculatorBottom label="+" onPress={() => console.log("HOLA CLIK")}  color={Colors.orange} />
+      </View>
+
+
+       {/*Cuarta Filas de Botones¨*/}
+
+      <View style={globalStyles.row}>
+        <CalculatorBottom label="7" onPress={() => console.log("HOLA CLIK")}   />
+        <CalculatorBottom label="8" onPress={() => console.log("HOLA CLIK")}   />
+        <CalculatorBottom label="9" onPress={() => console.log("HOLA CLIK")}   />
+        <CalculatorBottom label="-" onPress={() => console.log("HOLA CLIK")}  color={Colors.orange} />
+      </View>
+
+       {/*Cuarta Filas de Botones¨*/}
+
+      <View style={globalStyles.row}>
+        <CalculatorBottom label="0"  doubleSize onPress={() => console.log("HOLA CLIK")}   />
+        <CalculatorBottom label="." onPress={() => console.log("HOLA CLIK")}   />
+      
+        <CalculatorBottom label="="  onPress={() => console.log("HOLA CLIK")}  color={Colors.orange} />
+      </View>
+
+
+    </View>
+  );
+};
+
+export default Calculator;
